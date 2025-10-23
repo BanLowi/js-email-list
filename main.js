c31c1d168c1c041e45aa8c95db8e9f743259ac28
BanLowi <lowi40k@gmail.com>
2025-10-23 18:44:13 +0200

    fixed scoping and refresh button

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-let randomEmails = [];
 const formEl = document.querySelector("form");
+
+//Generazione button type=submit
+const submitEl = document.createElement("button");
+const typeSubmit = document.createAttribute("type");
+typeSubmit.value = "submit";
+submitEl.innerHTML = "Refresh";
+submitEl.setAttributeNode(typeSubmit);
 
 // Ciclo 10 volte l'endpoint e pusho il risultato nell'array
 for (let i = 0; i < 10; i++) {
-    
+
     const url = `https://flynn.boolean.careers/exercises/api/random/mail`;
     fetch(url)
         .then(res => res.json())
         .then(data => {
+            let randomEmails = [];
             const { success, response } = data;
-    
             randomEmails.push(response)
+
+            // Creo una lista con dentro
+            randomEmails.forEach(email => {
+                const listEl = document.createElement("ul");
+                listEl.innerHTML = `<li>${email}</li>`;
+                formEl.append(listEl);
+            });
+
+            formEl.append(submitEl);
         })
 
 }
 console.log(randomEmails);
 
-randomEmails.forEach(email => {
-    document.createElement("li")
-    
-});
