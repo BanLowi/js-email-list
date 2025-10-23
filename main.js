f2eb868c26494270804a89751a275ba3a75d369b
BanLowi <lowi40k@gmail.com>
2025-10-23 22:59:04 +0200

    added some styling

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const formEl = document.querySelector("form");
 
 // Genero una lista per il form
 const listEl = document.createElement("ul");
+listEl.classList.add("list-group", "mt-4")
 
 // Generazione button type=submit
 const submitEl = document.createElement("button");
 const typeSubmit = document.createAttribute("type");
 typeSubmit.value = "submit";
 submitEl.innerHTML = "Refresh";
+submitEl.classList.add("form-control", "btn", "btn-dark", "mt-4")
 submitEl.setAttributeNode(typeSubmit);
 
 // Ciclo 10 volte l'endpoint e pusho il risultato nell'array
@@ -27,6 +29,7 @@ for (let i = 0; i < 10; i++) {
             randomEmails.forEach(email => {
                 itemEl = document.createElement("li");
                 itemEl.textContent = email;
+                itemEl.classList.add("list-group-item")
                 listEl.append(itemEl);
             });
 
