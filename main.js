f07104df03463a3ec74a2ee4143af2f811f82f8d
BanLowi <lowi40k@gmail.com>
2025-10-26 22:35:50 +0100

    deleted forEach cycle and array in data promise

--- main.js
+++ main.js
@@ -15,23 +15,18 @@ submitEl.setAttributeNode(typeSubmit);
 // Ciclo 10 volte l'endpoint e pusho il risultato nell'array
 for (let i = 0; i < 10; i++) {
 
-    
+
     const url = `https://flynn.boolean.careers/exercises/api/random/mail`;
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            let randomEmails = [];
-            const { success, response } = data;
-            randomEmails.push(response)
-
             
+            const { success, response } = data;
 
-            randomEmails.forEach(email => {
-                itemEl = document.createElement("li");
-                itemEl.textContent = email;
-                itemEl.classList.add("list-group-item")
-                listEl.append(itemEl);
-            });
+            itemEl = document.createElement("li");
+            itemEl.textContent = response;
+            itemEl.classList.add("list-group-item")
+            listEl.append(itemEl);
 
             formEl.append(listEl);
             formEl.append(submitEl);
